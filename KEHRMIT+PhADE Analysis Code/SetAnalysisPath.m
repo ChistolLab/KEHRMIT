8c1d0f240231c8560ff815619b7ca9718335837b
ChistolLab <115442776+ChistolLab@users.noreply.github.com>
2022-11-01 15:47:15 -0700

    Delete KEHRMIT+PhADE Analysis Code directory

--- KEHRMIT+PhADE Analysis Code/SetAnalysisPath.m
+++
@@ -1,8 +0,0 @@
-function SetAnalysisPath()
-    StartPath =  '/Users/ghe/Dropbox (HMS)/aWalterLabFiles/_Daily_Electronic_Notes';
-    AnalysisPath = uigetdir(StartPath);
-    if ~isempty(AnalysisPath)
-        save 'AnalysisPath.mat' AnalysisPath;
-        disp(['Analysis Path - ' AnalysisPath]);
-    end
-end
--- KEHRMIT+PhADE Analysis Code/SetRawDataPath.m
+++
@@ -1,8 +0,0 @@
-function SetRawDataPath()
-    StartPath =  '/Users/ghe/Dropbox (HMS)/aWalterLabFiles/_Daily_Electronic_Notes';
-    RawDataPath = uigetdir(StartPath);
-    if ~isempty(RawDataPath)
-        save 'RawDataPath.mat' RawDataPath;
-        disp(['Raw Data - ' RawDataPath]);
-    end
-end
